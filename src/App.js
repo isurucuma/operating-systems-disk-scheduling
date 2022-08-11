067cddd054aed2742fe6f4e2f657b09df9c761e4
Miyushan <smiushan2015@gmail.com>
2022-08-11 22:25:07 +0530

    test

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import ButtonHolder from "./components/ButtonHolder";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Typography from '@mui/material/Typography';
 
 function App() {
     const [chartData, setChartData] = useState({ labels: null, arr: null });
     return (
-        <Container maxWidth="lg">
+        <Container maxWidth="" className="outerContainer">
+            <h1 margin="normal" className="topic">Disk Scheduling Algorithm</h1>
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container spacing={4}>
                     <Grid item xs={5}>
